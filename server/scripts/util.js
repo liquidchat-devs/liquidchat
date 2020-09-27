95e572571f436c486ad34fe5731732e59f7c0ac0
LamkasDev <filip.vujtik@email.cz>
2020-09-27 22:10:33 +0200

    fix file size check

--- server/scripts/util.js
+++ server/scripts/util.js
@@ -180,7 +180,7 @@ class Util {
             if(!this.isSessionValid(req, res)) { return; }
 
             var socket = this.app.sessionSockets.get(req.cookies['sessionID']);
-            var form = this.app.formidable({ multiples: true, maxFileSize: 1024 * 1024 * 100 });
+            var form = this.app.formidable({ multiples: true });
             form.uploadDir = this.app.filesStorage;
             form.keepExtensions = true;
         
@@ -191,23 +191,29 @@ class Util {
             var fileID = this.app.crypto.randomBytes(16).toString("hex");
             var fileID2 = fileID + (fileName.substring(fileName.lastIndexOf(".")))
             console.log("> received file - " + fileName)
-
-            form.on('error', (e) => {
-                socket.emit("uploadFail", fileID, fileName, fileSize);
-                return;
-            });
         
             form.on('progress', function(bytesReceived, bytesExpected) {
                 if(!sentStartPacket) {
                     sentStartPacket = true;
                     fileSize = bytesExpected;
-
                     socket.emit("uploadStart", fileID, fileName);
-                }
-        
-                socket.emit("uploadProgress", fileID, fileName, bytesReceived, bytesExpected);
-            }.bind(this));
 
+                    //File size check
+                    if (fileSize >= (1024 * 1024 * 100)) {
+                        form.emit('error', new Error(`File is too big-`));
+                        socket.emit("uploadFail", fileID, fileName, fileSize);
+                        return;
+                    }
+                }
+
+                //File size check
+                if (fileSize >= (1024 * 1024 * 100)) {
+                    return false;
+                } else {
+                    socket.emit("uploadProgress", fileID, fileName, bytesReceived, bytesExpected);
+                }
+            }.bind(this));
+        
             form.parse(req, async function(err, fields, files) {
                 if(files.fileUploaded === undefined) { return; }
                 this.app.fs.rename(files.fileUploaded.path, this.app.filesStorage + fileID2, function(err) {
