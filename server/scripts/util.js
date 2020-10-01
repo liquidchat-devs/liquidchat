3bacf6260b3f69f53555a67611f00e3e5a6716cc
LamkasDev <filip.vujtik@email.cz>
2020-10-01 17:10:49 +0200

    move file functions

--- server/scripts/util.js
+++ server/scripts/util.js
@@ -180,86 +180,15 @@ class Util {
         this.app.post('/upload', async(req, res) => {
             if(!this.isSessionValid(req, res)) { return; }
 
-            var socket = this.app.sessionSockets.get(req.cookies['sessionID']);
-            var form = this.app.formidable({ multiples: true, maxFileSize: 1024 * 1024 * 100 });
-            form.uploadDir = this.app.filesStorage;
-            form.keepExtensions = true;
-        
-            var fileName = req.query.fileName;
-            var fileSize = -1;
-            var sentStartPacket = false;
-
-            var fileID = this.app.crypto.randomBytes(16).toString("hex");
-            var fileID2 = fileID + (fileName.substring(fileName.lastIndexOf(".")))
-            console.log("> received file - " + fileName)
-        
-            form.on('progress', function(bytesReceived, bytesExpected) {
-                if(!sentStartPacket) {
-                    sentStartPacket = true;
-                    fileSize = bytesExpected;
-                    socket.emit("uploadStart", fileID, fileName);
-
-                    //File size check
-                    if (fileSize >= (1024 * 1024 * 100)) {
-                        form.emit('error', new Error(`File is too big-`));
-                        socket.emit("uploadFail", fileID, fileName, fileSize);
-                        return;
-                    }
-                }
-
-                //File size check
-                if (fileSize >= (1024 * 1024 * 100)) {
-                    return false;
-                } else {
-                    socket.emit("uploadProgress", fileID, fileName, bytesReceived, bytesExpected);
-                }
-            }.bind(this));
-        
-            form.parse(req, async function(err, fields, files) {
-                if(files.fileUploaded === undefined) { return; }
-                this.app.fs.rename(files.fileUploaded.path, this.app.filesStorage + fileID2, function(err) {
-                    if (err) { throw err; }
-                });
-
-                socket.emit("uploadFinish", fileID, fileName);
-                
-                var message = {
-                    text: fields.text,
-                    channel: {
-                        id: fields["channel.id"]
-                    },
-                    file: {
-                        name: fileID2,
-                        size: fileSize
-                    }
-                }
-                
-                await this.sendMessage(req, res, message)
-            }.bind(this));
+            await this.uploadFile(req, res)
+            console.log("> received file - " + req.query.fileName)
         })
 
         this.app.post('/updateAvatar', async(req, res) => {
             if(!this.isSessionValid(req, res)) { return; }
 
-            var session = this.app.sessions.get(req.cookies['sessionID']);
-            var user = await this.app.db.db_fetch.fetchUser(this.app.db, session.userID);
-            var form = this.app.formidable({ multiples: true });
-            form.uploadDir = this.app.filesStorage;
-            form.keepExtensions = true;
-        
-            var fileName = req.query.fileName;
-            var fileID = this.app.crypto.randomBytes(16).toString("hex");
-            var fileID2 = fileID + (fileName.substring(fileName.lastIndexOf(".")))
-            console.log("> received avatar - " + fileName)
-        
-            form.parse(req, async function(err, fields, files) {
-                this.app.fs.rename(files.fileUploaded.path, this.app.filesStorage + fileID2, function(err) {
-                    if (err) { throw err; }
-                });
-
-                user.avatar = fileID2
-                await this.updateUser(req, res, user)
-            }.bind(this));
+            await this.updateAvatar(req, res);
+            console.log("> received avatar update - " + req.query.fileName);
         });
 
         this.app.post('/sendFriendRequest', async(req, res) => {
@@ -721,6 +650,87 @@ class Util {
         })
 
         await this.app.db.db_edit.editUser(this.app.db, user);
+    }
+
+    async updateAvatar(req, res) {
+        var session = this.app.sessions.get(req.cookies['sessionID']);
+        var user = await this.app.db.db_fetch.fetchUser(this.app.db, session.userID);
+        var form = this.app.formidable({ multiples: true });
+        form.uploadDir = this.app.filesStorage;
+        form.keepExtensions = true;
+    
+        var fileName = req.query.fileName;
+        var fileID = this.app.crypto.randomBytes(16).toString("hex");
+        var fileID2 = fileID + (fileName.substring(fileName.lastIndexOf(".")))
+        console.log("> received avatar - " + fileName)
+    
+        form.parse(req, async function(err, fields, files) {
+            this.app.fs.rename(files.fileUploaded.path, this.app.filesStorage + fileID2, function(err) {
+                if (err) { throw err; }
+            });
+
+            user.avatar = fileID2
+            await this.updateUser(req, res, user)
+        }.bind(this));
+    }
+
+    async uploadFile(req, res) {
+        var socket = this.app.sessionSockets.get(req.cookies['sessionID']);
+        var form = this.app.formidable({ multiples: true, maxFileSize: 1024 * 1024 * 100 });
+        form.uploadDir = this.app.filesStorage;
+        form.keepExtensions = true;
+    
+        var fileName = req.query.fileName;
+        var fileSize = -1;
+        var sentStartPacket = false;
+
+        var fileID = this.app.crypto.randomBytes(16).toString("hex");
+        var fileID2 = fileID + (fileName.substring(fileName.lastIndexOf(".")))
+        console.log("> received file - " + fileName)
+    
+        form.on('progress', function(bytesReceived, bytesExpected) {
+            if(!sentStartPacket) {
+                sentStartPacket = true;
+                fileSize = bytesExpected;
+                socket.emit("uploadStart", fileID, fileName);
+
+                //File size check
+                if (fileSize >= (1024 * 1024 * 100)) {
+                    form.emit('error', new Error(`File is too big-`));
+                    socket.emit("uploadFail", fileID, fileName, fileSize);
+                    return;
+                }
+            }
+
+            //File size check
+            if (fileSize >= (1024 * 1024 * 100)) {
+                return false;
+            } else {
+                socket.emit("uploadProgress", fileID, fileName, bytesReceived, bytesExpected);
+            }
+        }.bind(this));
+    
+        form.parse(req, async function(err, fields, files) {
+            if(files.fileUploaded === undefined) { return; }
+            this.app.fs.rename(files.fileUploaded.path, this.app.filesStorage + fileID2, function(err) {
+                if (err) { throw err; }
+            });
+
+            socket.emit("uploadFinish", fileID, fileName);
+            
+            var message = {
+                text: fields.text,
+                channel: {
+                    id: fields["channel.id"]
+                },
+                file: {
+                    name: fileID2,
+                    size: fileSize
+                }
+            }
+            
+            await this.sendMessage(req, res, message)
+        }.bind(this));
     }
 
     convertTime(time) {
