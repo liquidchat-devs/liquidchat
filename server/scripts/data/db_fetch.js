292c789573b26b23ae089751000155ea47098acf
LamkasDev <filip.vujtik@email.cz>
2020-10-03 12:20:57 +0200

    fix logging in

--- server/scripts/data/db_fetch.js
+++ server/scripts/data/db_fetch.js
@@ -144,10 +144,10 @@ module.exports = {
         user.friendList = user.friendList.split(",")
         user.dmChannelList = user.dmChannelList.split(",")
 
-        if(containPassword !== true) {
+        if(containPassword != true) {
             delete user.password
         }
-        if(containSensitive !== true) {
+        if(containSensitive != true) {
             delete user.email
         }
         
